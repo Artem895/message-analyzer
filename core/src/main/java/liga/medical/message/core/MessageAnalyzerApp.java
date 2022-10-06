3ba635895be9480f053277f7b6e01a3c66035654
Artem365 <artiom.utyonoff@yandex.ru>
2022-10-06 14:43:50 +0300

    commit from git bash

---
+++ core/src/main/java/liga/medical/message/core/MessageAnalyzerApp.java
@@ -0,0 +1,15 @@
+package liga.medical.message.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"liga.medical.message"})
+public class MessageAnalyzerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageAnalyzerApp.class, args);
+    }
+
+}
